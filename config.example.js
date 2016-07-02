2ba4f1710bb470919836b1f80c24dd6c7a560f0b
araeisy <teleagentteam@gmail.com>
2016-07-02 15:15:48 +0430

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,10 @@
 var config = {
-  "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+  "token": "239879118:AAHSKNrc454lFH32UhKDJ4m7LA4gFv20yPw",
   "update_type": "long-polling", // or "webhook"
   "webhook": "https://www.example.com/<token>",
   //"proxy": "http://127.0.0.1:8118",
   //"bot_name": "example_bot", // check bot name,
-  "admin_id": "35197423"
+  "admin_id": "195801672"
 };
 
 module.exports = config;
